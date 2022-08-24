4b83cfdcafa30fe5c0806238d0d022723d1643c5
DESKTOP-H3M6PSM\khs55 <omfg6424@gmail.com>
2022-08-24 20:18:31 +0900

    Revert "임시 커밋"
    This reverts commit 98ac9fd86a5374223f9d81666b6efdfa4c248d7a.

--- src/components/pages/Create.jsx
+++ src/components/pages/Create.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import Responsive from '../common/Responsive';
-import CreateActionButtons from '../create/CreateActionButton';
-import Header from '../common/Header';
-// import CreateContainer from '../create/CreateContainer';
 import CreateGallery from '../create/CreateGallery';
 
 const Create = () => {
     return (
-        <>
-        <Header/>
-        <Responsive>
-          <CreateGallery />
-          <CreateActionButtons />
-        </Responsive>
-        </>
-        
-      );
+        <div>
+            <CreateGallery/>
+        </div>
+    );
 };
 
 export default Create;
